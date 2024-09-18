351cd3bafcbcf35e32c13615d837683dea4d485a
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-18 21:21:27 +0545

    Get Age and Name from User and Display in Python

---
+++ Day-4/Source-Code/Exercise2.py
@@ -0,0 +1,5 @@
+# Get Age and Name from User and Display
+print("Hi!, how are you?")
+name=input("Enter your Name: ")
+age=int(input("Enter your Age: "))
+print("Your name is: "+name+". And, your Age is: "+str(age))
