381d8e431c53295a6822886af5ab4e977bd9bcd3
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-18 21:23:24 +0545

    Demo of Input and Output Function in Python

---
+++ Day-4/Source-Code/InputOutput.py
@@ -0,0 +1,10 @@
+print("Hi, How are you?")
+name=input("Enter your name:")
+print(name)
+
+num1=input("First Number:")     #takes input as string
+num2=input("Second Number:")    #takes input as string
+print(num1+num2)        #concatenation of two strings 
+a=int(num1)     #typecasting(string->int)
+b=int(num2) #typecasting(string->int)
+print(a+b)
