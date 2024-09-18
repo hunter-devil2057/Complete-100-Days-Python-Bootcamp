e9c8956891ecb363f2fafeb49b597c3d592fe47c
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-18 21:25:35 +0545

    Python program that prompts the user to enter their name and age

---
+++ Day-5/Assignments/Assignment1.py
@@ -0,0 +1,4 @@
+# Write a Python program that prompts the user to enter their name and age.
+name=input("Enter Your Name: ")
+age=int(input("Enter Your Age:"))
+print(f"Hello, {name}. Nice to have you here...")
