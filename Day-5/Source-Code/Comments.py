8f6a18489b0e4d95c49e77a98ad31e935f94eca2
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-18 21:25:48 +0545

    Comments in Python

---
+++ Day-5/Source-Code/Comments.py
@@ -0,0 +1,13 @@
+# This code is written by me Manish Shiwakoti
+print("Hello world")
+
+a =  10 # giving value to a
+b = 3   # giving value to b
+c =  a + b  # adding a and b
+print(c)
+
+"""
+This is used to write comment in multiline 
+More you can write here....
+
+"""
