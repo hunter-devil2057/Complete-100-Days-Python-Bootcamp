acdb0784967b2d8630aac0f5eb266b70eaa06b41
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-18 21:20:31 +0545

    Hello World in Python

---
+++ Day-3/Source-Code/HelloWorld.py
@@ -0,0 +1,17 @@
+# Part 1
+print("Hello, World")
+print(2+2)
+print("hello"+" world")
+print(2-2)
+
+# Part 2
+print("hello world")
+print("hello world program")
+print(2+3)
+
+
+#   Part 3. 
+print("hello world")
+print(2+3)
+print("How are you dear")
+print("Hello" + " World")
