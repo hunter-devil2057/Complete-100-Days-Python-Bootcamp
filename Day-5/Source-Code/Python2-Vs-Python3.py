524475e7af601a9aa49fc8deceea1210c40d363a
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-18 21:18:33 +0545

    Python 2 Versus Python 3

---
+++ Day-5/Source-Code/Python2-Vs-Python3.py
@@ -0,0 +1,20 @@
+print("This is a day 05")
+print ("Hello world")
+print(5/2)
+input("Enter your Name: ")
+for i in range(1, 11, 1):
+        print(i)
+
+""""
+In Python2 print "hello world"
+In Python3 print("hello world")
+
+In Python2 int a = 5/2 ->2
+In Python3 int a = 5/2 ->2.5
+
+In Python2, raw_input()
+In Python3, input()
+
+In Python2, xrange method
+In Python3, range method
+"""
