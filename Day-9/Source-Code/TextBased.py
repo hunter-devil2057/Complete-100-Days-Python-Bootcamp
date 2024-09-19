c42bad508c5010bbe5479847ce3d95b8e8827bd2
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-19 19:45:55 +0545

    Text-based Data Type in Python

---
+++ Day-9/Source-Code/TextBased.py
@@ -0,0 +1,23 @@
+'''
+String: collection of numbers, alphabetic lowercase and uppercase, special number, etc. in the single or double quotation, are considered to be a string. 
+Example: 
+Str1="How are you"
+Str2='How are you'
+Str3="""How are you"""
+Str1="How are you"
+Without assigning to a variable, it will consider a comment as, 
+"""Multiline comment,"""
+'''
+print("String type of Data in Python")
+str1="Manish Shiwakoti"
+str2="salfl*(*#)kjlds0934isdf"
+str3='Manish Shiwakoti'
+str4="""Devilz Hunter"""
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+"""
+Multi
+Line 
+Comment"""
