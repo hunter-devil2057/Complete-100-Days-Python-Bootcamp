f19b54c873b58997db786dda2cf281d1a3b29b9c
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-19 18:56:09 +0545

    Sequence and Non-Sequence Data Types in Python

---
+++ Day-8/Source-Code/SequentialNonSequential.py
@@ -0,0 +1,15 @@
+lst = ['red','green','yellow','black','pink','white','gray']
+print(lst[-2])
+
+tpl = ('red','green','yellow','black','pink','white','gray')
+print(tpl[1])
+
+name = "Python is programming language"
+print(name[10])
+
+set = {3,4,5,6,7,12}
+print(set)
+
+d = {'name':'manish','age':23}
+print(d['age'])
+
