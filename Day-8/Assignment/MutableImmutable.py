db87eea0843fc67f344dac07140c9672b7c8ae25
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-19 19:02:01 +0545

    Mutable and Immutable Data Types in Python

---
+++ Day-8/Assignment/MutableImmutable.py
@@ -0,0 +1,12 @@
+# list is the mutable
+list = [2,3,4,5,6,7]
+list[2] = 10
+print(list[2])
+print(list)
+
+# tuple is the immutable, int, float, bool, string
+name = "Manish Shiwakoti"
+name[0] = "d"
+tuple = (2,3,4,54,6)
+#tuple[0]  = 10
+#print(tuple)
