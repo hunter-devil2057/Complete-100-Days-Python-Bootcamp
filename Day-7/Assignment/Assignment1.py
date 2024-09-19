a876203e78088e46010a3bb43f5bd6e80e8b570c
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-19 17:58:42 +0545

    Assignment on Escape Sequence

---
+++ Day-7/Assignment/Assignment1.py
@@ -0,0 +1,21 @@
+'''
+Write a Python program that prints the following text exactly as shown below:
+
+Hello, Python!
+I'm learning escape sequences.
+\t - This is a tab.
+\n - This is a newline.
+\\ - This is a backslash.
+Example Output:
+
+Hello, Python!
+I'm learning escape sequences.
+\t - This is a tab.
+\n - This is a newline.
+\\ - This is a backslash.
+'''
+print("Hello, Python!")
+print("I'm learning escape sequences.")
+print("\\t - This is a tab.")
+print("\\n - This is a tab.")
+print("\\ - This is a tab.")
