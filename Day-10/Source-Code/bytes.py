ae1d87205d3a4681404de504e7931d6b5febc92b
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:42:32 +0545

    Bytes in Python

---
+++ Day-10/Source-Code/bytes.py
@@ -0,0 +1,14 @@
+# site = "ManishShiwakoti.com"
+# byte = bytes(site, 'utf-16')
+# print(byte)
+
+# n = 3
+# byte = bytes(n)
+# print(byte)
+
+# byte = bytes()
+# print(byte)
+
+lst = [1,2,3]
+byte = bytes(lst)
+print(byte)
