d4bd702ab6dbf0a27f2d5f7f3ebc6ec8135ee3c9
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:54:55 +0545

    Exercise on Assignment Operators

---
+++ Day-20/Source-Code/AssignmentExercise1.py
@@ -0,0 +1,5 @@
+# to get a number from user
+# increase one value to that number 
+num = int(input("Enter a number")) # '4' -> 4
+num += 1 # num + 1
+print(num)
---
+++ Day-20/Source-Code/AssogmentExercise2.py
@@ -0,0 +1,5 @@
+print("Ex 2 to decrease a number from user entered number")
+num =  int(input("Enter a number")) # 4
+print("Your number is "+str(num))
+num -= 1
+print("Decreased number is "+str(num))
