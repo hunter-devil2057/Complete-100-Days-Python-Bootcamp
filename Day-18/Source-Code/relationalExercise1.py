b6ffbd92cb3276e424fa90b7dfc2a655268f6f50
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:48:03 +0545

    Exercise related to Relational Operator in Python

---
+++ Day-18/Source-Code/relationalExercise1.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter 1st number"))
+n2 = int(input("Enter 2nd number"))
+
+if(n1 == n2):
+    print("Both are equal")
+else:
+    print("Not equal")
---
+++ Day-18/Source-Code/relationalExercise2.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter 1st number")) # 30
+n2 = int(input("Enter 2nd number")) # 10
+
+if(n1 > n2):
+    print(n1)
+else:
+    print(n2)
