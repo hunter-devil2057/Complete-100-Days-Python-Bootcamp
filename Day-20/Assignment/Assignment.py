63f81ea18d62f1055c34a1a1a103ddd3f00c637e
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:56:09 +0545

    Assignment on Assignment Operator

---
+++ Day-20/Assignment/Assignment.py
@@ -0,0 +1,25 @@
+# Assignment Operator
+'''
+Create a variable num with an initial value of 10.
+Perform the following assignment operations and print the result after each operation:
+    Add 5 to num using the += operator.
+    Subtract 3 from num using the -= operator.
+    Multiply num by 2 using the *= operator.
+    Divide num by 4 using the /= operator.
+    Raise num to the power of 3 using the **= operator.
+'''
+num=10
+num+=5
+print(num)
+
+num-=3
+print(num)
+
+num*=2
+print(num)
+
+num/=4
+print(num)
+
+num**=3
+print(num)
