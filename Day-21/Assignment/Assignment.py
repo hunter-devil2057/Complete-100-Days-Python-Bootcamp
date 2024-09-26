140f74fa49ff106da92b51b80250925bf408a852
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:52:44 +0545

    Assignment on Logical Operations

---
+++ Day-21/Assignment/Assignment.py
@@ -0,0 +1,29 @@
+# Logical Operations
+'''
+Create two boolean variables x and y with values True and False respectively.
+
+Perform the following logical operations and store the results in variables:
+    Logical AND: Perform a logical AND operation between x and y and store the result in a variable named and_result.
+    Logical OR: Perform a logical OR operation between x and y and store the result in a variable named or_result.
+    Logical NOT: Perform a logical NOT operation on x and store the result in a variable named not_x.
+    Logical NOT: Perform a logical NOT operation on y and store the result in a variable named not_y.
+
+Print the results of all the operations.
+'''
+x=True; y=False
+
+# logical And
+and_result=x and y
+print(and_result)
+
+# logical OR
+or_result=x or y
+print(or_result)
+
+# logical not x
+not_x= not x
+print(not_x)
+
+# logical not y
+not_y=not y
+print(not_y)
