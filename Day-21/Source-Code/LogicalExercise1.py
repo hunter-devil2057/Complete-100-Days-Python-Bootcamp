a26c38481b0ec7d8d703aba3c691f6a240ce2b9f
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:51:28 +0545

    Exercise 1 on Logical Operator in Python

---
+++ Day-21/Source-Code/LogicalExercise1.py
@@ -0,0 +1,9 @@
+username = "manishshiwakoti123"
+pwd = "manish123"
+name = input("Enter your username")
+password = input("Enter your password")
+
+if(username == name and pwd == password ):
+    print("Your username and password is correct")
+else:
+    print("Sorry, username or password or both incorrect")
