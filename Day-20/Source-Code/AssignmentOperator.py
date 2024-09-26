80affdc0fc71815a26047e0fd08d49574dd0fc83
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:55:23 +0545

    AssignmentOperator in Python

---
+++ Day-20/Source-Code/AssignmentOperator.py
@@ -0,0 +1,15 @@
+print("Assignment operator in Python")
+
+x = 10
+x += 20 # x = x + 20, 10 + 20= 30, x =  30
+print(x) # 30
+x -= 10 # x = x - 10, 30-10
+print(x) # 20
+x += 10
+print(x)
+x *= 3 # x = x * 3, 30 * 3
+print(x)
+x /= 3
+print(x)
+x %= 3
+print(x)
