ac6e9350c9fe76323247160999b894794ff336bc
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:40:38 +0545

    Dictionary in Python

---
+++ Day-10/Source-Code/dictionary.py
@@ -0,0 +1,10 @@
+dict={"name":"Manish Shiwakoti",
+      "age":32,
+      "Course":"Python"}
+
+print(dict)
+print(type(dict))
+# getting the value of name 
+print(dict["name"])
+print(dict["age"])
+print(dict["Course"])
