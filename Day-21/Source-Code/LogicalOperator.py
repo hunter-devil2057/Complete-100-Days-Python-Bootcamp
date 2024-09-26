c2c87231467d1e3f9eb7dc9425ce5f717af3477c
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:51:02 +0545

    Logical Operator in Python

---
+++ Day-21/Source-Code/LogicalOperator.py
@@ -0,0 +1,17 @@
+print("We are learning Logical Operators working in the Python")
+a = 10
+b = 20
+c = 14
+d = 17
+# and 
+print(a>b and c<b ) # False
+# false and fasle = false
+# false and true = false
+# true and false = false
+# true and true = true
+print(a>b or c>b) # False
+# false or fasle = false
+# false or true = true
+# true or false = true
+# true or true = true
+print(not(a>b))
