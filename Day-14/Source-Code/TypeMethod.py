ad960b17c96e758b144f6e32b0f8932738f2ed31
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 21:46:22 +0545

    type( )  method in Python

---
+++ Day-14/Source-Code/TypeMethod.py
@@ -0,0 +1,20 @@
+print("How are you!")
+
+# type method 
+a = 3
+print(type(a))
+
+b = 3.4
+print(type(b))
+
+c = "Manish Shiwakoti"
+print(type(c))
+
+d = "3.3" # string
+print(type(d))
+
+l = [3,4,54,5,6,7]
+print(type(l))
+
+t = (3,4,54,5,6,7)
+print(type(t))
