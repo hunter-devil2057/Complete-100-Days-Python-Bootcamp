aca5629ff40eb00d487c166e6f394988f537937c
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-26 22:00:32 +0545

    Exercises on Arithmetic Operator in Python

---
+++ Day-17/Source-Code/ArithmeticExercise1.py
@@ -0,0 +1,7 @@
+v1 = int(input("Enter 1st number")) # '5' to 5
+v2 = int(input("Enter 2nd number"))# '3' to 3
+print(v1 + v2)
+print(v1 - v2)
+print(v1 / v2)
+print(v1 * v2)
+print(v1 % v2)
---
+++ Day-17/Source-Code/ArithmeticExercise2.py
@@ -0,0 +1,7 @@
+v1 = int(input("Enter 1st number"))
+v2 = int(input("Enter 2nd number"))
+v3 = int(input("Enter 3rd number"))
+v4 = int(input("Enter 4th number"))
+r1 = v1 - v2
+r2 = v3 + v4
+print(r1*r2) # 4, 3, 2, 5
