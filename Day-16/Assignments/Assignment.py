a25e1322a6362b731ca5485c059578abbb0a1796
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2024-09-27 10:11:53 +0545

    Basic Operators in Python

---
+++ Day-16/Assignments/Assignment.py
@@ -0,0 +1,29 @@
+# Basic Operators
+'''
+Create two variables num1 and num2 with values 10 and 5 respectively.
+
+Perform the following operations:
+    Addition: Add num1 and num2 and store the result in a variable named add_result.
+    Subtraction: Subtract num2 from num1 and store the result in a variable named sub_result.
+    Multiplication: Multiply num1 and num2 and store the result in a variable named mul_result.
+    Division: Divide num1 by num2 and store the result in a variable named div_result.
+    Exponentiation: Raise num1 to the power of num2 and store the result in a variable named exp_result.
+
+Print the results of all the operations.
+'''
+
+num1=10; num2=5
+add_result=num1+num2
+print(add_result)
+
+sub_result=num1-num2
+print(sub_result)
+
+mul_result=num1*num2
+print(mul_result)
+
+div_result=num1/num2
+print(div_result)
+
+exp_result=num1**num2
+print(exp_result)
